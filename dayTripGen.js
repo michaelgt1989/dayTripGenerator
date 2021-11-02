6b3cfca0fa707c9c2b1c4f7144f803ccedf3f79b
Michael George-Taylor <michael.gt1989@gmail.com>
2021-11-01 21:46:26 -0600

    final personal touches

--- dayTripGen.js
+++ dayTripGen.js
@@ -1,9 +1,11 @@
 'make strict'
+
+alert("Welcome! You are about to generate a random day trip. Press 'Enter' or click 'OK' to get started.")
 // list that hold all the options 
 let destination = ["Dallas", "Oklahoma City", "Denver", "Detroit", "Kansas City", "Cleveland"];
 let restaurant = ["Olive Garden", "Chili's", "Village Inn", "Outback", "Taco Bell", "Arby's", "iHop"];
 let transportation = ["walking", "driving", "riding a bicycle", "catching an Uber", "taking a limo", "skateboarding", "cruising in a boat"];
-let entertainment = ["see a movie!", "an amusement park!", "a painting workshop!", "a dance club!", "a renaissance fair! *Swords clink*", "a slam poetry session!", "feed some ducks! *quack*"];
+let entertainment = ["see a movie. May I suggest a rom-com?", "an amusement park! WOOOO!", "a painting workshop. Let's get our 'Bob Ross' on!", "a dance club! It takes two to tango.", "a renaissance fair! Don't forget thy armor!", "a slam poetry session! It's about to get deep.", "a pond to feed some ducks! *quack*"];
 
 //these variables will hold the random choice 
 let selectedDestination =  randomGenerator(destination)
@@ -32,56 +34,46 @@ alert("You will go to " + selectedDestination + "," + " you will eat at " + sele
 userOptions()
 function userOptions(){
     input = prompt("Do you like your trip? Enter yes to confirm the trip, or if you'd like to select a new destination enter 1, if you'd like to select a new restaurant enter 2, if you'd like a different mode of transportation enter 3, or if you want a different choice of entertainment for the evening enter 4.")
-if (input === "1"){
+
+
+if (input === "yes"){
+    alert("Your trip has been added to the console. Now go have some fun! Press refresh to try the whole thing again.")
+    console.log("The user will go to " + selectedDestination + "," + " the user will eat at " + selectedRestaurant + "," + " the user will get there by " + selectedTransportation + " and, in the evening the user will go to " + selectedEntertainment)
+}
+else if (input === "Yes"){
+    alert("Your trip has been added to the console. Now go have some fun! Press refresh to try the whole thing again.")
+    console.log("The user will go to " + selectedDestination + "," + " the user will eat at " + selectedRestaurant + "," + " the user will get there by " + selectedTransportation + " and, in the evening the user will go to " + selectedEntertainment)
+}
+else if (input === "1"){
     selectedDestination = randomGenerator(destination)
     let finishedList = [selectedDestination, selectedRestaurant, selectedTransportation, selectedEntertainment];
-    alert("Your destination will now be " + selectedDestination)
+    alert("Your destination will now be " + selectedDestination + ".")
     alert("You will go to " + selectedDestination + "," + " you will eat at " + selectedRestaurant + "," + " you will get there by " + selectedTransportation + " and, in the evening you will go to " + selectedEntertainment)
     userOptions(finishedList)
 }
 else if (input === "2"){
     selectedRestaurant = randomGenerator(restaurant)
     let finishedList = [selectedDestination, selectedRestaurant, selectedTransportation, selectedEntertainment];
-    alert("You will now eat at " + selectedRestaurant)
+    alert("You will now eat at " + selectedRestaurant + ".")
     alert("You will go to " + selectedDestination + "," + " you will eat at " + selectedRestaurant + "," + " you will get there by " + selectedTransportation + " and, in the evening you will go to " + selectedEntertainment)
     userOptions(finishedList)
 }
 else if (input === "3"){
     selectedTransportation = randomGenerator(transportation)
     let finishedList = [selectedDestination, selectedRestaurant, selectedTransportation, selectedEntertainment];
-    alert("You will now get there by " + selectedTransportation)
+    alert("You will now get there by " + selectedTransportation + ".")
     alert("You will go to " + selectedDestination + "," + " you will eat at " + selectedRestaurant + "," + " you will get there by " + selectedTransportation + " and, in the evening you will go to " + selectedEntertainment)
     userOptions(finishedList)
 }
 else if (input === "4"){
     selectedEntertainment = randomGenerator(entertainment)
     let finishedList = [selectedDestination, selectedRestaurant, selectedTransportation, selectedEntertainment];
-    alert("In the evening you will now go to " + selectedEntertainment)
+    alert("In the evening you will now go to " + selectedEntertainment + ".")
     alert("You will go to " + selectedDestination + "," + " you will eat at " + selectedRestaurant + "," + " you will get there by " + selectedTransportation + " and, in the evening you will go to " + selectedEntertainment)
     userOptions(finishedList)
 }
-else if (input === "yes"){
-    alert("Your trip has been added to the console. Now go have fun :)")
-    console.log("The user will go to " + selectedDestination + "," + " the user will eat at " + selectedRestaurant + "," + " the user will get there by " + selectedTransportation + " and, in the evening the user will go to " + selectedEntertainment)
-}
-else if (input === "Yes"){
-    alert("You said yes with a capital Y. That was not even an option you rule breaker! Nevertheless, your trip has been added to the console and now it is time to GO HAVE FUN!")
-    console.log("The user will go to " + selectedDestination + "," + " the user will eat at " + selectedRestaurant + "," + " the user will get there by " + selectedTransportation + " and, in the evening the user will go to " + selectedEntertainment)
+else if (input !== "yes", "Yes", "1", "2", "3", "4"){
+    alert("Let's try that again. Please enter yes, 1, 2, 3 or 4.")
+    userOptions(finishedList)
 }
 }
-
-
-
-
-
-
-
-// make a function that can randomly pick an item from an array
-// look in the previous worksheets for clues or do quick online research
-
-// now we need to display each random selection to the user
-// for this we will ust console.log()
-
-
-
-
